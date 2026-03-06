6ad2f4e5188fe9a73e486a949f04c4d197bcf753
Onur Topaloglu <topalogluonur53@gmail.com>
2026-03-06 14:53:45 +0300

    yeniden kurulum

--- finans_backend/config/settings.py
+++ finans_backend/config/settings.py
@@ -98,7 +98,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('SQLITE_DB_PATH', BASE_DIR / 'db.sqlite3'),
+        'NAME': os.environ.get('SQLITE_DB_PATH', '/DATA/database/db.sqlite3'),
     }
 }
 
--- finans_backend/entrypoint.sh
+++ finans_backend/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Ensure data directory exists
 mkdir -p /app/data
+# Create the directory for the SQLite database if it's set
+if [ -n "$SQLITE_DB_PATH" ]; then
+    mkdir -p $(dirname "$SQLITE_DB_PATH")
+fi
 
 # Apply database migrations
 python manage.py migrate --noinput
